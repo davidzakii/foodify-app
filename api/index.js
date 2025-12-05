cbc14f7a72b3a0d70b389866f2ba387e7aea3eeb
davidzakii <davidzaki77@gmail.com>
2025-12-05 16:22:15 +0200

    commit 16 : change build:ssr

--- api/index.js
+++ api/index.js
@@ -1,4 +1,2 @@
-import { createNodeRequestHandler } from '@angular/ssr/node';
-import server from '../dist/foodify-app/server/server.mjs';
-
-export const reqHandler = createNodeRequestHandler(server);
+import { reqHandler } from '../dist/foodify-app/server/server.mjs';
+export default reqHandler;
